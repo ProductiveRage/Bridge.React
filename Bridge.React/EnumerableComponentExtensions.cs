6698f441dc1ab5b86179dc5079c22e6900db3028
ProductiveRage <dangger36@gmail.com>
2016-02-11 18:37:33 +0000

    Added ToChildComponentArray extension methods
    It's easier to use these extension methods when creating sets of child
    components than it is to remember to cast each child component to
    Any<ReactElement, string> and to then call ToArray on the set

---
+++ Bridge.React/EnumerableComponentExtensions.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Bridge.React
+{
+	public static class EnumerableComponentExtensions
+	{
+		/// <summary>
+		/// When initialising a component that will accept a set of child components, each child components must be of type Any&lt;ReactElement, string&gt; - if you have an enumerable
+		/// set of ReactElements then calling ToArray will not return an array of the appropriate type, so either each entry must be cast to an Any&lt;ReactElement, string&gt; before
+		/// calling ToArray or this helper function may be used.
+		/// </summary>
+		public static Any<ReactElement, string>[] ToChildComponentArray(this IEnumerable<ReactElement> elements)
+		{
+			if (elements == null)
+				throw new ArgumentNullException("elements");
+
+			return elements
+				.Select(component => (Any<ReactElement, string>)component)
+				.ToArray();
+		}
+
+		/// <summary>
+		/// When initialising a component that will accept a set of child components, each child components must be of type Any&lt;ReactElement, string&gt; - if you have an enumerable
+		/// set of PureComponents of the same type then this helper function may be called to produce an array of the correct type (otherwise, each entry must be cast to an
+		/// Any&lt;ReactElement, string&gt; before ToArray is called on that set)
+		/// </summary>
+		public static Any<ReactElement, string>[] ToChildComponentArray<TProps>(this IEnumerable<PureComponent<TProps>> components)
+		{
+			if (components == null)
+				throw new ArgumentNullException("components");
+
+			return components
+				.Select(component => (Any<ReactElement, string>)component)
+				.ToArray();
+		}
+
+		/// <summary>
+		/// When initialising a component that will accept a set of child components, each child components must be of type Any&lt;ReactElement, string&gt; - if you have an enumerable
+		/// set of PureComponents of the same type then this helper function may be called to produce an array of the correct type (otherwise, each entry must be cast to an
+		/// Any&lt;ReactElement, string&gt; before ToArray is called on that set)
+		/// </summary>
+		public static Any<ReactElement, string>[] ToChildComponentArray<TProps>(this IEnumerable<StatelessComponent<TProps>> components)
+		{
+			if (components == null)
+				throw new ArgumentNullException("components");
+
+			return components
+				.Select(component => (Any<ReactElement, string>)component)
+				.ToArray();
+		}
+	}
+}
