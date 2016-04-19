86f4cac4b7e39d0a0c7e1aace502c3117c4575f0
ProductiveRage <dangger36@gmail.com>
2016-04-19 21:03:10 +0100

    Dispatching a message during the handling of another is not allowed, in order to be consistent with the Facebook Dispatcher

--- Bridge.React/Dispatcher/AppDispatcher.cs
+++ Bridge.React/Dispatcher/AppDispatcher.cs
@@ -2,31 +2,59 @@
 
 namespace Bridge.React
 {
-    public sealed class AppDispatcher
-    {
-        private event Action<DispatcherMessage> _dispatcher;
+	public sealed class AppDispatcher
+	{
+		private event Action<DispatcherMessage> _dispatcher;
 
-        public void Register(Action<DispatcherMessage> callback)
-        {
-            _dispatcher += callback;
-        }
+		private bool _currentDispatching;
+		public AppDispatcher()
+		{
+			_currentDispatching = false;
+		}
 
-        public void HandleViewAction(IDispatcherAction action)
-        {
-            if (action == null)
-                throw new ArgumentNullException("action");
 
+		public void Register(Action<DispatcherMessage> callback)
+		{
+			_dispatcher += callback;
+		}
+
+		public void HandleViewAction(IDispatcherAction action)
+		{
+			if (action == null)
+				throw new ArgumentNullException("action");
+
+			Dispatch(new DispatcherMessage(MessageSourceOptions.View, action));
+		}
+
+		public void HandleServerAction(IDispatcherAction action)
+		{
+			if (action == null)
+				throw new ArgumentNullException("action");
+
+			Dispatch(new DispatcherMessage(MessageSourceOptions.Server, action));
+		}
+
+		private void Dispatch(DispatcherMessage message)
+		{
+			if (message == null)
+				throw new ArgumentNullException("message");
+
+			// Dispatching a message during the handling of another is not allowed, in order to be consistent with the Facebook Dispatcher
+			// (see https://github.com/facebook/flux/blob/master/src/Dispatcher.js#L183)
 			if (_dispatcher != null)
-				_dispatcher(new DispatcherMessage(MessageSourceOptions.View, action));
-        }
-
-        public void HandleServerAction(IDispatcherAction action)
-        {
-            if (action == null)
-                throw new ArgumentNullException("action");
-
-			if (_dispatcher != null)
-				_dispatcher(new DispatcherMessage(MessageSourceOptions.Server, action));
-        }
-    }
+			{
+				if (_currentDispatching)
+					throw new Exception("Cannot dispatch in the middle of a dispatch.");
+				_currentDispatching = true;
+				try
+				{
+					_dispatcher(message);
+				}
+				finally
+				{
+					_currentDispatching = false;
+				}
+			}
+		}
+	}
 }
