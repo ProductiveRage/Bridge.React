333a6ee1efec7b3da7885f0213bd79c68c66fda8
ProductiveRage <dangger36@gmail.com>
2016-06-17 08:11:15 +0100

    Removed "DefaultSelected" and "Disable" from OptionAttributes since they both seem to be ignored by React

--- Bridge.React/Attributes/OptionAttributes.cs
+++ Bridge.React/Attributes/OptionAttributes.cs
@@ -5,8 +5,6 @@ namespace Bridge.React
 	[ObjectLiteral]
     public sealed class OptionAttributes : ReactDomElementAttributes<OptionElement>
     {
-        public bool DefaultSelected { private get; set; }
-        public bool Disable { private get; set; }
         public string Label { private get; set; }
         public string Value { private get; set; }
     }
