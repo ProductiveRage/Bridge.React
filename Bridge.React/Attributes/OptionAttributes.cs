a0ffc0b04b22652a486d7fac610bd3712d6041a2
ProductiveRage <dangger36@gmail.com>
2016-06-17 08:08:12 +0100

    Removed "Text" from OptionAttributes because it's ignored by React (set a child component string instead)

--- Bridge.React/Attributes/OptionAttributes.cs
+++ Bridge.React/Attributes/OptionAttributes.cs
@@ -2,14 +2,13 @@ using Bridge.Html5;
 
 namespace Bridge.React
 {
-    [ObjectLiteral]
+	[ObjectLiteral]
     public sealed class OptionAttributes : ReactDomElementAttributes<OptionElement>
     {
         public bool DefaultSelected { private get; set; }
         public bool Disable { private get; set; }
         public string Label { private get; set; }
         public bool Selected { private get; set; }
-        public string Text { private get; set; }
         public string Value { private get; set; }
     }
 }
