5cb35f5173a911e487bf40013c53c8c991af8445
ProductiveRage <dangger36@gmail.com>
2016-06-17 08:09:35 +0100

    Removed "Selected" from OptioAttributes since React warns to use "defaultValue" or "value" on the parent select element

--- Bridge.React/Attributes/OptionAttributes.cs
+++ Bridge.React/Attributes/OptionAttributes.cs
@@ -8,7 +8,6 @@ namespace Bridge.React
         public bool DefaultSelected { private get; set; }
         public bool Disable { private get; set; }
         public string Label { private get; set; }
-        public bool Selected { private get; set; }
         public string Value { private get; set; }
     }
 }
