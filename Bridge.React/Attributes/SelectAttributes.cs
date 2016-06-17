cb8dd5f47ab997417f6d3433e1ade98ee727cb5f
ProductiveRage <dangger36@gmail.com>
2016-06-17 08:15:42 +0100

    Added Values property to SelectAttribute for cases where Multiple is true
    It would be nice to have an analyser that warned you before runtime if
    you were using the wrong property, but that's an idea for another day

--- Bridge.React/Attributes/SelectAttributes.cs
+++ Bridge.React/Attributes/SelectAttributes.cs
@@ -1,20 +1,26 @@
-using Bridge.Html5;
-
 namespace Bridge.React
 {
-    [ObjectLiteral]
-    public sealed class SelectAttributes : ReactDomElementAttributes<SelectElement>
-    {
-        [Name("autofocus")]
-        public bool AutoFocus { private get; set; }
-        public bool Disabled { private get; set; }
-        public int Length { private get; set; }
-        public bool Multiple { private get; set; }
-        public string Name { private get; set; }
-        public bool Required { private get; set; }
-        public int Size { private get; set; }
-        public string Src { private get; set; }
-        public string Type { private get; set; }
-        public string Value { private get; set; }
-    }
+	[ObjectLiteral]
+	public sealed class SelectAttributes : ReactDomElementAttributes<HTMLSelectElement>
+	{
+		[Name("autofocus")]
+		public bool AutoFocus { private get; set; }
+		public bool Disabled { private get; set; }
+		public int Length { private get; set; }
+		public bool Multiple { private get; set; }
+		public string Name { private get; set; }
+		public bool Required { private get; set; }
+		public int Size { private get; set; }
+		public string Src { private get; set; }
+		public string Type { private get; set; }
+		/// <summary>
+		/// This property should be used (instead of Values) if Multiple is false
+		/// </summary>
+		public string Value { private get; set; }
+		/// <summary>
+		/// This property should be used (instead of Value) if Multiple is true
+		/// </summary>
+		[Name("value")]
+		public string[] Values { private get; set; }
+	}
 }
