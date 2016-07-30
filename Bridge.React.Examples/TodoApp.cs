d371379860dad15e1472342a0973c40a17f9dd6a
ProductiveRage <dangger36@gmail.com>
2016-07-30 14:14:19 +0100

    Tweaks to sample app to prevent the React warning - Each child in an array or iterator should have a unique "key" prop
    Two changes were required. The dynamic elements (the individual TODO
    cards) need to have a Key value on them, which is easy enough since each
    Todo instance has an Id property (though I changed this to always
    increment, rather than taking the count of the Todo list, to ensure that
    values are not reused when items are removed and then new ones added).
    The non-dynamic elements (the header and the input form) are now
    specified using the original method signature, which is not intended for
    use with dynamic elements and so the warning is avoided (there is
    potential for a naughty workaround to avoid the dynamic children warning
    by passing a pre-populated array reference instead of listing the static
    elements one-by-one as params - I'd like to look into adding an analyser
    to detect this).

--- Bridge.React.Examples/TodoApp.cs
+++ Bridge.React.Examples/TodoApp.cs
@@ -1,154 +1,148 @@
 ﻿using System;
-using System.Linq;
-using Bridge.React;
 using System.Collections.Generic;
-using Bridge.Html5;
+using System.Linq;
 
 namespace Bridge.React.Examples
 {
-    public class TodoApp : Component<TodoApp.Props, TodoApp.State>
-    {
-        public TodoApp(string label)
-            : base(new Props { Label = label }) { }
+	public class TodoApp : Component<TodoApp.Props, TodoApp.State>
+	{
+		private int _nextAvailableId = 0;
+		public TodoApp(string label) : base(new Props { Label = label }) { }
 
-        protected override State GetInitialState()
-        {
-            return new State
-            {
-                InputValue = "",
-                Todos = new Todo[]
-                {
-                    new Todo { Id = 0, Description = "Learn C#", Done = true },
-                    new Todo { Id = 1, Description = "Learn React", Done = false },
-                    new Todo { Id = 2, Description = "Build an awesome app with C# and React", Done = false }
-                }
-            };
-        }
+		protected override State GetInitialState()
+		{
+			return new State
+			{
+				InputValue = "",
+				Todos = new Todo[]
+				{
+					new Todo { Id = _nextAvailableId++, Description = "Learn C#", Done = true },
+					new Todo { Id = _nextAvailableId++, Description = "Learn React", Done = false },
+					new Todo { Id = _nextAvailableId++, Description = "Build an awesome app with C# and React", Done = false }
+				}
+			};
+		}
 
-        private State AppendTodo(string todoDescription)
-        {
-            var count = state.Todos.Count();
-            var todo = new Todo
-            {
-                Description = todoDescription,
-                Done = false,
-                Id = count + 1
-            };
+		private State AppendTodo(string todoDescription)
+		{
+			var count = state.Todos.Count();
+			var todo = new Todo
+			{
+				Description = todoDescription,
+				Done = false,
+				Id = _nextAvailableId++
+			};
 
-            return new State
-            {
-                InputValue = "",
-                Todos = state.Todos.Append(todo)
-            };
-        }
+			return new State
+			{
+				InputValue = "",
+				Todos = state.Todos.Append(todo)
+			};
+		}
 
-        private State RemoveTodo(int id)
-        {
-            return new State
-            {
-                InputValue = state.InputValue,
-                Todos = state.Todos.Where(todo => todo.Id != id)
-            };
-        }
+		private State RemoveTodo(int id)
+		{
+			return new State
+			{
+				InputValue = state.InputValue,
+				Todos = state.Todos.Where(todo => todo.Id != id)
+			};
+		}
 
-        private State ToggleDone(int id)
-        {
-            var todos = state.Todos.ToArray();
-            for(int i = 0; i < todos.Length; i++)
-            {
-                if (todos[i].Id == id)
-                {
-                    todos[i].Done = !todos[i].Done;
-                }
-            }
+		private State ToggleDone(int id)
+		{
+			var todos = state.Todos.ToArray();
+			for(int i = 0; i < todos.Length; i++)
+			{
+				if (todos[i].Id == id)
+				{
+					todos[i].Done = !todos[i].Done;
+				}
+			}
 
-            return new State
-            {
-                InputValue = state.InputValue,
-                Todos = todos
-            };
-        }
+			return new State
+			{
+				InputValue = state.InputValue,
+				Todos = todos
+			};
+		}
 
-        private IEnumerable<T> ListOf<T>(params T[] items)
-        {
-            return items;
-        }
+		public override ReactElement Render()
+		{
+			return DOM.Div(new Attributes { ClassName = "wrapper" },
+				DOM.H3(props.Label),
+				DOM.Label("Description"),
+				DOM.Input(new InputAttributes
+				{
+					Value = state.InputValue,
+					OnChange = e => SetState(new State { InputValue = e.CurrentTarget.Value, Todos = state.Todos })
+				}),
+				DOM.Button(
+					new ButtonAttributes
+					{
+						Disabled = string.IsNullOrWhiteSpace(state.InputValue),
+						OnClick = e => SetState(AppendTodo(state.InputValue))
+					},
+					"Add"
+				),
+				DOM.Div(
+					state.Todos
+						 .Select(todo =>
+								DOM.H4(new Attributes { ClassName = todo.Done ? "done" : "not-done" }, todo.Description),
+								DOM.Button(
+									new ButtonAttributes
+									{
+										ClassName = todo.Done ? "toggle-done" : "toggle-not-done",
+										OnClick = e => SetState(ToggleDone(todo.Id))
+									},
+									todo.Done ? "Not done yet!" : "Finished!"
+								),
+								DOM.Button(
+									new ButtonAttributes
+									{
+										ClassName = "remove-btn",
+										OnClick = e => SetState(RemoveTodo(todo.Id))
+									},
+									"Remove"
+								)
+						 ))
+						 .Pipe(DOM.Div)
+				)
+			);
+		}
 
-        public override ReactElement Render()
-        {
-            return DOM.Div(new Attributes { ClassName = "wrapper" },
-                DOM.Div(ListOf(
-                    DOM.H3(props.Label),
-                    DOM.Label("Description"),
-                    DOM.Input(new InputAttributes
-                    {
-                        Value = state.InputValue,
-                        OnChange = e => SetState(new State { InputValue = e.CurrentTarget.Value, Todos = state.Todos })
-                    }),
-                    DOM.Button(
-                    new ButtonAttributes
-                    {
-                        Disabled = string.IsNullOrWhiteSpace(state.InputValue),
-                        OnClick = e => SetState(AppendTodo(state.InputValue))
-                    },
-                    "Add"
-                ))),
-                state.Todos
-                     .Select(todo =>
-                        DOM.Div(new Attributes { ClassName = "todo-container" },
-                            DOM.H4(new Attributes
-                            {
-                                ClassName = todo.Done ? "done" : "not-done"
-                            }, todo.Description),
-                            DOM.Button(new ButtonAttributes
-                            {
-                                ClassName = todo.Done ? "toggle-done" : "toggle-not-done",
-                                OnClick = e => SetState(ToggleDone(todo.Id))
-                            }, todo.Done ? "Not done yet!" : "Finished!"),
-                            DOM.Button(new ButtonAttributes
-                            {
-                                ClassName = "remove-btn",
-                                OnClick = e => SetState(RemoveTodo(todo.Id))
-                            }, "Remove")
-                     ))
-                     .Pipe(DOM.Div)
-            );
-        }
+		[ObjectLiteral]
+		public class Props
+		{
+			public string Label;
+		}
 
-        [ObjectLiteral]
-        public class Props
-        {
-            public string Label;
-        }
+		[ObjectLiteral]
+		public class State
+		{
+			public string InputValue;
+			public IEnumerable<Todo> Todos;
+		}
 
-        [ObjectLiteral]
-        public class State
-        {
-            public string InputValue;
-            public IEnumerable<Todo> Todos;
-        }
+		[ObjectLiteral]
+		public class Todo
+		{
+			public int Id { get; set; }
+			public string Description { get; set; }
+			public bool Done { get; set; }
+		}
+	}
 
-        [ObjectLiteral]
-        public class Todo
-        {
-            public int Id { get; set; }
-            public string Description { get; set; }
-            public bool Done { get; set; }
-        }
-    }
+	public static class Extenstions
+	{
+		public static U Pipe<T, U>(this T value, Func<T, U> f)
+		{
+			return f(value);
+		}
 
-
-
-    public static class Extenstions
-    {
-        public static U Pipe<T, U>(this T value, Func<T, U> f)
-        {
-            return f(value);
-        }
-
-        public static IEnumerable<T> Append<T>(this IEnumerable<T> source, T value)
-        {
-            return source.Concat(new T[] { value });
-        }
-    }
+		public static IEnumerable<T> Append<T>(this IEnumerable<T> source, T value)
+		{
+			return source.Concat(new T[] { value });
+		}
+	}
 }
