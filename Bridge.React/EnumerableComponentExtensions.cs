5b98ae6f60ab1fa8af44b3e7e6978ec245d149d0
ProductiveRage <dangger36@gmail.com>
2016-07-30 14:19:22 +0100

    Marked ToChildComponentArray methods as obsolete now that the DOM element factory methods support enumerable sets of elements
    ToChildComponentArray resulted in the React 'Each child in an array or
    iterator should have a unique "key" prop' warning being bypassed, which
    is not good (React needs unique keys for dynamic children in order to
    operate efficiently and corectly). The new element factory methods will
    allow this warning to be reported, which might mean that warnings will
    start appearing where there were none before, but they were being
    incorrectly masked. The ToChildComponentArray methods will likely be
    removed in a future release.

--- Bridge.React/EnumerableComponentExtensions.cs
+++ Bridge.React/EnumerableComponentExtensions.cs
@@ -11,6 +11,7 @@ namespace Bridge.React
 		/// set of ReactElements then calling ToArray will not return an array of the appropriate type, so either each entry must be cast to an Any&lt;ReactElement, string&gt; before
 		/// calling ToArray or this helper function may be used.
 		/// </summary>
+		[Obsolete("This method is not required now that the DOM element factory methods have overloads that will take enumerable sets of elements. Also, this method prevents the React warning 'Each child in an array or iterator should have a unique \"key\" prop' from being raised, which is too important to be bypassed. As such, use of this method is not recommended and it will likely be removed in a future version of this library.")]
 		public static Any<ReactElement, string>[] ToChildComponentArray(this IEnumerable<ReactElement> elements)
 		{
 			if (elements == null)
@@ -26,6 +27,7 @@ namespace Bridge.React
 		/// set of PureComponents of the same type then this helper function may be called to produce an array of the correct type (otherwise, each entry must be cast to an
 		/// Any&lt;ReactElement, string&gt; before ToArray is called on that set)
 		/// </summary>
+		[Obsolete("This method is not required now that the DOM element factory methods have overloads that will take enumerable sets of elements. Also, this method prevents the React warning 'Each child in an array or iterator should have a unique \"key\" prop' from being raised, which is too important to be bypassed. As such, use of this method is not recommended and it will likely be removed in a future version of this library.")]
 		public static Any<ReactElement, string>[] ToChildComponentArray<TProps>(this IEnumerable<PureComponent<TProps>> components)
 		{
 			if (components == null)
@@ -41,6 +43,7 @@ namespace Bridge.React
 		/// set of PureComponents of the same type then this helper function may be called to produce an array of the correct type (otherwise, each entry must be cast to an
 		/// Any&lt;ReactElement, string&gt; before ToArray is called on that set)
 		/// </summary>
+		[Obsolete("This method is not required now that the DOM element factory methods have overloads that will take enumerable sets of elements. Also, this method prevents the React warning 'Each child in an array or iterator should have a unique \"key\" prop' from being raised, which is too important to be bypassed. As such, use of this method is not recommended and it will likely be removed in a future version of this library.")]
 		public static Any<ReactElement, string>[] ToChildComponentArray<TProps>(this IEnumerable<StatelessComponent<TProps>> components)
 		{
 			if (components == null)
