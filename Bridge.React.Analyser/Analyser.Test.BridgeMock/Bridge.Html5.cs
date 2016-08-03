f422d78f7157922cd842004d8d455578e0c83fba
ProductiveRage <dangger36@gmail.com>
2016-08-03 21:46:49 +0100

    Changed the "Style" property on ReactDomElementAttributes from being dynamic to using the new ReactStyle class
    This could be a breaking change if anyone has used other classes to
    represent inline styles in the past - however, since there has been an
    issue reported about setting inline styles I don't think that this is a
    huge risk (so I'm going to say that this fixes #8)

--- Bridge.React.Analyser/Analyser.Test.BridgeMock/Bridge.Html5.cs
+++ Bridge.React.Analyser/Analyser.Test.BridgeMock/Bridge.Html5.cs
@@ -5,8 +5,79 @@
 		public class Element { }
 		public class HTMLSelectElement : Element { }
 
+		public enum All { }
+		public enum AlignContent { }
+		public enum AlignItems { }
+		public enum AnimationDirection { }
+		public enum AnimationFillMode { }
+		public enum AnimationPlayState { }
+		public enum BackfaceVisibility { }
+		public enum BackgroundBlendMode { }
+		public enum BackgroundClip { }
+		public enum BackgroundRepeat { }
+		public enum BorderImageRepeat { }
+		public enum BorderCollapse { }
+		public enum BorderStyle { }
+		public enum BorderWidth { }
+		public enum BoxDecorationBreak { }
+		public enum BoxSizing { }
+		public enum CaptionSide { }
+		public enum Clear { }
+		public enum ColumnFill { }
+		public enum ColumnSpan { }
 		public enum ContentEditable { }
+		public enum Cursor { }
+		public enum Direction { }
+		public enum Display { }
+		public enum DominantBaseline { }
+		public enum EmptyCells { }
+		public enum FlexDirection { }
+		public enum FlexWrap { }
+		public enum Float { }
+		public enum FontKerning { }
+		public enum FontStretch { }
+		public enum FontStyle { }
+		public enum FontSynthesis { }
+		public enum FontVariant { }
+		public enum FontVariantCaps { }
+		public enum FontVariantLigatures { }
+		public enum FontVariantPosition { }
+		public enum GridAutoFlow { }
+		public enum Hyphens { }
+		public enum ImageRendering { }
+		public enum ImeMode { }
+		public enum JustifyContent { }
+		public enum ListStylePosition { }
+		public enum ListStyleType { }
+		public enum MaskType { }
+		public enum ObjectFit { }
+		public enum Overflow { }
+		public enum PageBreak { }
+		public enum PageBreakInside { }
+		public enum PointerEvents { }
+		public enum Position { }
+		public enum Resize { }
+		public enum TableLayout { }
+		public enum TextAlign { }
+		public enum TextDecoration { }
+		public enum TextDecorationLine { }
+		public enum TextDecorationStyle { }
 		public enum TextDirection { }
+		public enum TextOverflow { }
+		public enum TextRendering { }
+		public enum TextTransform { }
+		public enum TextUnderlinePosition { }
+		public enum TimingFunction { }
+		public enum TouchAction { }
+		public enum TransformStyle { }
+		public enum UnicodeBidi { }
+		public enum VerticalAlign { }
+		public enum Visibility { }
+		public enum WhiteSpace { }
+		public enum WordBreak { }
+		public enum WordWrap { }
+		public enum WritingMode { }
+
 
 		public class CompositionEvent<T> { }
 		public class FocusEvent<T> { }
