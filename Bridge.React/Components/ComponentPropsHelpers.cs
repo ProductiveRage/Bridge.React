e51b98a3365ee6a2f0d1b1275b1727ec3b58e86d
ProductiveRage <dangger36@gmail.com>
2016-08-28 15:26:38 +0100

    Added alternative component creation mechanism that enables greater performance for huge numbers of elements
    The StaticComponent class may be used to generate React stateless
    elements (either pure - meaning that their content varies only by their
    props value and that the props values support shallow comparisons - or
    just stateless - meaning that their content may vary by other data or
    that their props type does not support shallow comparison). Since static
    functions are used for rendering instead of class instances, there is
    less overhead in the rendering, which may be important for pages that
    have to support updating 1000s of elements. In the future, the
    PureComponent and StatelessComponent classes *may* be marked as
    obsolete, with a recommendation to use static function variations
    instead.

--- Bridge.React/Components/ComponentPropsHelpers.cs
+++ Bridge.React/Components/ComponentPropsHelpers.cs
@@ -8,6 +8,7 @@
 	/// value from the props, since this must not be tucked away one level deeper as it is a magic React property (for more information
 	/// about keyed elements, see https://facebook.github.io/react/docs/multiple-components.html#dynamic-children).
 	/// </summary>
+	[IgnoreGeneric]
 	internal static class ComponentPropsHelpers<TProps>
 	{
 		internal static WrappedProps WrapProps(TProps propsIfAny)
@@ -43,6 +44,62 @@
 			if (Script.Write<bool>("(typeof(keyIfAny) !== 'undefined')"))
 				return new WrappedPropsWithKey { Value = propsIfAny, Key = keyIfAny };
 			return new WrappedProps { Value = propsIfAny };
+		}
+
+		internal static bool DoPropsReferencesMatch(TProps props1, TProps props2)
+		{
+			if ((props1 == null) && (props2 == null))
+				return true;
+			else if ((props1 == null) || (props2 == null))
+				return false;
+
+			if (props1.GetType() != props2.GetType())
+				return false;
+
+			/*@
+			for (var propName in props1) {
+				var propValue1 = props1[propName];
+				var propValue2 = props2[propName];
+				if ((propValue1 === propValue2) 
+				|| ((propValue1 === null) && (propValue2 === null))
+				|| ((typeof(propValue1) === "undefined") && (typeof(propValue2) === "undefined"))) {
+					// Very simple cases where the properties match
+					continue;
+				}
+				else if ((propValue1 === null) || (propValue2 === null) || (typeof(propValue1) === "undefined") || (typeof(propValue2) === "undefined")) {
+					// Simple cases where one or both of the values are some sort of no-value (but either one of them has a value or they're inconsistent types of no-value,
+					// since we'd have caught them above otherwise)
+					return false;
+				}
+				else if ((typeof(propValue1) === "function") && (typeof(propValue2) === "function")) {
+					// If they're Bridge-bound functions (which is what the presence of $scope and $method properties indicates), then check whether the underlying $method
+					// and $scope references match (if they do then this means that it's the same method bound to the same "this" scope, but the actual function references
+					// are not the same since they were the results from two different calls to Bridge.fn.bind)
+					if (propValue1.$scope && propValue1.$method && propValue2.$scope && propValue2.$method && (propValue1.$scope === propValue2.$scope) && (propValue1.$method === propValue2.$method)) {
+						continue;
+					}
+					// Due to the way that properties are currently initialised on types in Bridge, if a property's type is a struct then the getter and setter for it will
+					// be created for each instance of the type, rather than being shared across all instances of the type (which is the case for reference type properties).
+					// This means that when it comes to a "getName" property, for example, the "getName" function will not be the same value for two instances of the same
+					// class, which is a problem for this function since it will mean that two props references that contain the same data are not identified as such as
+					// the getter / setter functions are distinct across instances. A proper fix for this has been requested at:
+					//   http://forums.bridge.net/forum/general/feature-requests/1737
+					// A temporary workaround is for the getter and setter functions to be identified as such, and then ignored by this process. That would not be particularly
+					// easy to do in general, but it IS something that's relatively easy to add to the ProductiveRage.Immutable library (every time that CtorSet is called, the
+					// getter and setter methods for the property will have a $scaffolding value set to true). This can be unpicked if the Bridge translation process changes
+					// but it means that types that have struct properties that are declared using the IAmImmutable helpers will work before that time.
+					if ((propValue1.$scaffolding === true) && (propValue2.$scaffolding === true)) {
+						continue;
+					}
+				}
+				else if ((typeof(propValue1.equals) === "function") && (propValue1.equals(propValue2) === true)) {
+					// If propValue1 has an "equals" implementation then give that a go
+					continue;
+				}
+				return false;
+			}
+			*/
+			return true;
 		}
 
 		[ObjectLiteral]
---
+++ Bridge.React/Components/StaticComponent.cs
@@ -0,0 +1,66 @@
+﻿using System;
+
+namespace Bridge.React
+{
+	/// <summary>
+	/// While the class-based component structure (using the PureComponent and StatelessComponent base classes) is very convenient and feels natural, there is some overhead to
+	/// constructing the component instances. For the vast majority of the time, this will probably not cause any problems. However, if you have a page where you may need to
+	/// update 1000s of elements at a time then this construction cost may become non-neligible. An alternative is to use static render methods instead of component classes.
+	/// The methods in this class make that possible - the render methods used provided must take a single props argument and return a ReactElement. If the props type supports
+	/// shallow comparison for change detection (which is highly recommended but often requires immutable types to be used for all properties) then the Pure method should be
+	/// used; this will result in a component with a ShouldComponentUpdate implementation that will tell React not to re-render if the props data hasn't changed. If the props
+	/// type does not support shallow comparison then the Stateless method should be used; this uses a lighter weight structure to create the React element but there is no
+	/// way to support a ShouldComponentUpdate mechanism.
+	/// </summary>
+	public static class StaticComponent
+	{
+		/// <summary>
+		/// Use this if the props type supports shallow comparison (which generally requires immutable types to be used for all of the props values) - the resulting component
+		/// will automatically be assigned a ShouldComponentUpdate function so that re-renders of the component may be avoided if the props data has not changed.
+		/// </summary>
+		public static ReactElement Pure<TProps>(Func<TProps, ReactElement> renderer, TProps props)
+		{
+			/*@
+			var componentClass = renderer.$$componentClass;
+			if (!componentClass) {
+				var doPropsReferencesMatch = this.doPropsReferencesMatch;
+				componentClass = React.createClass({
+					displayName: renderer.name,
+					render: function () {
+						return renderer(this.props.value);
+					},
+					shouldComponentUpdate: function (nextProps, nextState) {
+						return !doPropsReferencesMatch(this.props ? this.props.value : null, nextProps ? nextProps.value : null);
+					}
+				});
+				renderer.$$componentClass = componentClass;
+			}
+			*/
+			var wrappedProps = ComponentPropsHelpers<TProps>.WrapProps(props);
+			return Script.Write<ReactElement>("React.createElement(componentClass, wrappedProps)");
+		}
+
+		/// <summary>
+		/// Use this if the props type does not support shallow comparisons
+		/// </summary>
+		public static ReactElement Stateless<TProps>(Func<TProps, ReactElement> renderer, TProps props)
+		{
+			/*@
+			var namedScopeBoundFunction;
+			eval("namedScopeBoundFunction = function " + renderer.name + "(props) { return renderer(props ? props.value : props); };");
+			*/
+			var wrappedProps = ComponentPropsHelpers<TProps>.WrapProps(props);
+			return Script.Write<ReactElement>("React.createElement(namedScopeBoundFunction, wrappedProps)");
+		}
+
+		/// <summary>
+		/// This method is just here to make it easier for the native JavaScript in the method above to call the static function in the ComponentPropsHelpers
+		/// class without us having to bake in the way that Bridge represents static functions on classes
+		/// </summary>
+		[IgnoreGeneric]
+		internal static bool DoPropsReferencesMatch<TProps>(TProps props1, TProps props2)
+		{
+			return ComponentPropsHelpers<TProps>.DoPropsReferencesMatch(props1, props2);
+		}
+	}
+}
