7adb7550efca82f8a0ae93def214c7cf90a736db
ProductiveRage <dangger36@gmail.com>
2016-11-07 17:58:50 +0000

    Bumper version number

--- SharedAssemblyInfo.cs
+++ SharedAssemblyInfo.cs
@@ -1,5 +1,5 @@
 ﻿using System.Reflection;
 
 [assembly: AssemblyCopyright("Copyright © ProductiveRage 2016")]
-[assembly: AssemblyVersion("1.8.8.0")]
-[assembly: AssemblyFileVersion("1.8.8.0")]
+[assembly: AssemblyVersion("1.8.9.0")]
+[assembly: AssemblyFileVersion("1.8.9.0")]
