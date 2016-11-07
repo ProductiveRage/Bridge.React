22228260542d83496b204578fe01cf97d89e7862
ProductiveRage <dangger36@gmail.com>
2016-11-07 17:56:27 +0000

    Removed the "Value" property from the ButtonAttribute class since it's not used (child elements set the content)

--- Bridge.React/Attributes/ButtonAttributes.cs
+++ Bridge.React/Attributes/ButtonAttributes.cs
@@ -2,19 +2,18 @@ using Bridge.Html5;
 
 namespace Bridge.React
 {
-    [ObjectLiteral]
-    public sealed class ButtonAttributes : ReactDomElementAttributes<HTMLButtonElement>
-    {
-        [Name("autofocus")]
-        public bool AutoFocus { private get; set; }
-        public bool Disabled { private get; set; }
-        public string FormAction { private get; set; }
-        public string FormEncType { private get; set; }
-        public string FormMethod { private get; set; }
-        public bool FormNoValidate { private get; set; }
-        public string FormTarget { private get; set; }
-        public string Name { private get; set; }
-        public ButtonType Type { private get; set; }
-        public string Value { private get; set; }
-    }
+	[ObjectLiteral]
+	public sealed class ButtonAttributes : ReactDomElementAttributes<HTMLButtonElement>
+	{
+		[Name("autofocus")]
+		public bool AutoFocus { private get; set; }
+		public bool Disabled { private get; set; }
+		public string FormAction { private get; set; }
+		public string FormEncType { private get; set; }
+		public string FormMethod { private get; set; }
+		public bool FormNoValidate { private get; set; }
+		public string FormTarget { private get; set; }
+		public string Name { private get; set; }
+		public ButtonType Type { private get; set; }
+	}
 }
