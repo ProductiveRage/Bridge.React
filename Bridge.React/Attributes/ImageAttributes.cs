32dcb669b822a2e86ddb1cd186b83559606d4845
ProductiveRage <dangger36@gmail.com>
2016-12-07 17:38:57 +0000

    Added "Alt" property to ImageAttributes (fixes #45)

--- Bridge.React/Attributes/ImageAttributes.cs
+++ Bridge.React/Attributes/ImageAttributes.cs
@@ -2,15 +2,16 @@
 
 namespace Bridge.React
 {
-    [ObjectLiteral]
-    public sealed class ImageAttributes : ReactDomElementAttributes<HTMLImageElement>
-    {
-        public string CrossOrigin { private get; set; }
-        public int Height { private get; set; }
-        public bool IsMap { private get; set; }
-        public string Src { private get; set; }
-        public string SrcSet { private get; set; }
-        public string UseMap { private get; set; }
-        public int Width { private get; set; }
-    }
+	[ObjectLiteral]
+	public sealed class ImageAttributes : ReactDomElementAttributes<HTMLImageElement>
+	{
+		public string Alt { private get; set; }
+		public string CrossOrigin { private get; set; }
+		public int Height { private get; set; }
+		public bool IsMap { private get; set; }
+		public string Src { private get; set; }
+		public string SrcSet { private get; set; }
+		public string UseMap { private get; set; }
+		public int Width { private get; set; }
+	}
 }
