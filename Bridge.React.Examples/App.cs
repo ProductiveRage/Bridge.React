5448a5c902bf3322e3bd1b68fc7e0c4b828c2df4
ProductiveRage <dangger36@gmail.com>
2017-01-10 17:33:53 +0000

    Version bump for release (v1.12.1)

--- Bridge.React.Examples/App.cs
+++ Bridge.React.Examples/App.cs
@@ -1,19 +1,27 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using Bridge.React;
+﻿using System.Linq;
 using Bridge.Html5;
 
 namespace Bridge.React.Examples
 {
-    public class App
+	public class App
     {
         public static void Main()
         {
-            React.Render(
-                new TodoApp("C# React canonical todo app"),
+			var values = new[] { 1, 2, 3 };
+
+			var test = ReactElementList.Empty
+				.Add(DOM.Div(new Attributes { Key = "i1" }, "abc"))
+				.Add(values.Select(value => DOM.Div(new Attributes { Key = "value-" + value }, value.ToString())))
+				.Add(DOM.Div(new Attributes { Key = "i9" }, "xyz"));
+
+
+
+
+
+
+			React.Render(
+                //new TodoApp("C# React canonical todo app"),
+				DOM.Div(test),
                 Document.GetElementById("main")
             );
         }
