4886d5a5e2a645d1c4abfb7a84de1796336563f8
ProductiveRage <dangger36@gmail.com>
2017-01-10 17:14:35 +0000

    Bumped version number for release (v1.12.0)

--- SharedAssemblyInfo.cs
+++ SharedAssemblyInfo.cs
@@ -1,5 +1,5 @@
 ﻿using System.Reflection;
 
-[assembly: AssemblyCopyright("Copyright © ProductiveRage 2016")]
-[assembly: AssemblyVersion("1.11.0.0")]
-[assembly: AssemblyFileVersion("1.11.0.0")]
+[assembly: AssemblyCopyright("Copyright © ProductiveRage 2017")]
+[assembly: AssemblyVersion("1.12.0.0")]
+[assembly: AssemblyFileVersion("1.12.0.0")]
