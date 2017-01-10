cea6e5923774b1e675389c978a91d27fc3e05bf3
ProductiveRage <dangger36@gmail.com>
2017-01-10 17:35:33 +0000

    Included an incorrect file in the last change, reverted and pushed the correct one

--- Bridge.React.Examples/App.cs
+++ Bridge.React.Examples/App.cs
@@ -1,29 +1,15 @@
-﻿using System.Linq;
-using Bridge.Html5;
+﻿using Bridge.Html5;
 
 namespace Bridge.React.Examples
 {
 	public class App
-    {
-        public static void Main()
-        {
-			var values = new[] { 1, 2, 3 };
-
-			var test = ReactElementList.Empty
-				.Add(DOM.Div(new Attributes { Key = "i1" }, "abc"))
-				.Add(values.Select(value => DOM.Div(new Attributes { Key = "value-" + value }, value.ToString())))
-				.Add(DOM.Div(new Attributes { Key = "i9" }, "xyz"));
-
-
-
-
-
-
+	{
+		public static void Main()
+		{
 			React.Render(
-                //new TodoApp("C# React canonical todo app"),
-				DOM.Div(test),
-                Document.GetElementById("main")
-            );
-        }
-    }
+				new TodoApp("C# React canonical todo app"),
+				Document.GetElementById("main")
+			);
+		}
+	}
 }
