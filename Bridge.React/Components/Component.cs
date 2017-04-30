5e8f3dfbb1d6de8ebe6ee44eff34f3993285ebbc
ProductiveRage <dangger36@gmail.com>
2017-04-30 18:22:12 +0100

    Rewrote React component initialisation for compatibility with 16.0
    React 16.0 won't support React.createClass so need to bring to
    initialise components in a similar way to ES6 class-based components do.
    A side effect of this is that PureComponent will support the full set of
    lifecycle methods as Component does, it seems unlikely that
    optimisations will appear that enable components to support
    shouldComponentUpdate without also supporting the full lifecycle.

--- Bridge.React/Components/Component.cs
+++ Bridge.React/Components/Component.cs
@@ -7,7 +7,8 @@ namespace Bridge.React
 	/// <summary>
 	/// This provides a class which may be derived from in order to declare React components. Due to the way that React initialises components, it is important that derived classes
 	/// do not perform any logic or initialisation in their constructor, nor may they have any other configuration passed into their constructor but that which is described by the
-	/// props (and state, where applicable) data. The constructors will not be executed and so any logic or member initialisation in there will be silenty ignored.
+	/// props (and state, where applicable) data. The constructors will not be executed and so any logic or member initialisation in there will be silenty ignored (this is due to
+	/// the fact that C# has to be able to support multiple constructor overloads but JavaScript does not and React is built on this assumption).
 	/// </summary>
 	public abstract class Component<TProps, TState>
 	{
@@ -22,7 +23,7 @@ namespace Bridge.React
 			object reactComponentClass;
 			if (!_reactComponentClasses.TryGetValue(currentType, out reactComponentClass))
 			{
-				reactComponentClass = CreateReactComponentClass();
+				reactComponentClass = ReactComponentClassCreator.CreateClass(this);
 				_reactComponentClasses[currentType] = reactComponentClass;
 			}
 
@@ -35,68 +36,6 @@ namespace Bridge.React
 			if (children != null)
 				createElementArgs = createElementArgs.Concat(children);
 			_reactElement = Script.Write<ReactElement>("React.createElement.apply(null, createElementArgs)");
-		}
-
-		private object CreateReactComponentClass()
-		{
-			// For each derived class we need to create React component class, this is like a template for the form of the component - individual instances will be created by
-			// taking this template and using React.createElement to prepare a new element with the specified props. There should only be one React component class per unique
-			// class but multiple React elements - one per instance of the class. The template is prepared by taking the first instance of any component class and ensuring
-			// promoting all functions from the class prototype (which is where they will be defined for Bridge classes) since React's internal logic uses hasOwnProperty when
-			// looking for key functions such as "render". Most functions can be promoted in a simple manner but there are some exceptions - "constructor" must be ignored
-			// (otherwise React will raise a warning "You are attempting to define `constructor` on your component more than once" and "setState") and any of the component
-			// lifecycle methods that involves props or state access need special handling. This is because React presumes that all props and state objects will be simple
-			// object literals and they get picked apart during processing - so if a Bridge class is used for the props object that has a "name" property with "getName" and
-			// "setName" functions on the prototype then React will maintain the "name" value but lose the getter and setter. The way around this is to stash the props object
-			// in a simple wrapper with a "value" property which React will allow through its internals since it doesn't recursively meddle with the data - this "value" may be
-			// a Bridge class (or an ObjectLiteral, it doesn't matter). The only complications around this approach are that anywhere that props are accessed, a redirection is
-			// required - so that the component class gets the "value" reference. This counts not only for components' "this.props" access but also for functions such as
-			// "componentWillReceiveProps" since the C# code will expect the "nextProps" value to be of type TProps and not a wrapper with a "value" property of type TProps.
-			// The same logic goes for the state reference. Note that this effectively means that state is completely replaced when the "SetState" function is called, since the
-			// single "value" reference is over-written. This makes the "SetState" implementation here closer to "replaceState" in React, but I didn't name it "ReplaceState"
-			// instead of "SetState" since the React 0.14 docs say that that function may be removed in the future. However, "SetState" replacing the entire state reference,
-			// rather than merging it with whatever data is already there is - I think - the least surprising approach when considered in terms of C#; it makes more sense to
-			// replace the current data with the new reference, rather than merge - merging is not a common action in C#, though it is in JavaScript (eg. merging default
-			// "options" with any explicit settings in many JavaScript APIs).
-			var className = ComponentNameHelpers.GetDisplayName(this);
-			object reactComponentClass = null;
-			/*@
-			var bridgeComponentInstance = this;
-			bridgeComponentInstance.displayName = className; // This is used by the React dev tools extension
-				
-			// Copy over all functions that may be needed first (ignoring the constructor since copying that causes a Reacts warning and because the constructor will not
-			// be used when createElement initialises new element instances)..
-			for (var i in bridgeComponentInstance) {
-				if (i === 'constructor') {
-					continue;
-				}
-				bridgeComponentInstance[i] = bridgeComponentInstance[i];
-			}
-
-			// .. then overwrite the functions that need special treatment (lifecycle functions involving props and/or state)
-			var getInitialState = bridgeComponentInstance.getInitialState;
-			bridgeComponentInstance.getInitialState = function (state) {
-				return { value: getInitialState.apply(this) };
-			};
-			var componentWillReceiveProps = bridgeComponentInstance.componentWillReceiveProps;
-			bridgeComponentInstance.componentWillReceiveProps = function (nextProps) {
-				componentWillReceiveProps.apply(this, [ nextProps ? nextProps.value : nextProps ]);
-			};
-			var shouldComponentUpdate = bridgeComponentInstance.shouldComponentUpdate;
-			bridgeComponentInstance.shouldComponentUpdate = function (nextProps, nextState) {
-				return shouldComponentUpdate.apply(this, [ nextProps ? nextProps.value : nextProps, nextState ? nextState.value : nextState ]);
-			};
-			var componentWillUpdate = bridgeComponentInstance.componentWillUpdate;
-			bridgeComponentInstance.componentWillUpdate = function (nextProps, nextState) {
-				componentWillUpdate.apply(this, [ nextProps ? nextProps.value : nextProps, nextState ? nextState.value : nextState ]);
-			};
-			var componentDidUpdate = bridgeComponentInstance.componentDidUpdate;
-			bridgeComponentInstance.componentDidUpdate = function (previousProps, previousState) {
-				componentDidUpdate.apply(this, [ previousProps ? previousProps.value : previousProps, previousState ? previousState.value : previousState ]);
-			};
-			reactComponentClass = React.createClass(bridgeComponentInstance);
-			*/
-			return reactComponentClass;
 		}
 
 		/// <summary>
@@ -113,7 +52,7 @@ namespace Bridge.React
 		/// </summary>
 		protected TState state
 		{
-			// If tate is non-null then it needs to be "unwrapped" when the C# code requests it
+			// If state is non-null then it needs to be "unwrapped" when the C# code requests it
 			get { return Script.Write<TState>("this.state ? this.state.value : null"); }
 		}
 
@@ -150,8 +89,14 @@ namespace Bridge.React
 		}
 
 		/// <summary>
-		/// State is not used by all components and so it is valid to return null from any implementation of this function
+		/// When writing React components in JavaScript, it is recommended that they be constructed as classes that are derived from React.Component and that any initial-state setting be
+		/// done in the constructor (for this reason, GetInitialState is not support for JavaScript class-based React components). However, JavaScript does not have to support multiple
+		/// constructor signatures, unlike C#, which makes things easier for JavaScript React; with these bindings, multiple constructors signatures ARE supported but they will not be
+		/// called during component initialisation and so must not contain any code (there is an analyser to identify any time that this rule has been forgotten). Since constructors
+		/// are not executed, the GetInitialState is still the way to set initial state. Note: State is not used by all components and so it is valid to return null from any
+		/// implementation of this function.
 		/// </summary>
+		[Name("constructorStateInitialiser")] // "getInitialState" is not supported for class-based components so rename this to something else and tie it in via ReactComponentClassCreator.InitialiseComponentState
 		protected virtual TState GetInitialState() { return default(TState); }
 
 		protected virtual void ComponentWillMount() { }
---
+++ Bridge.React/Components/ReactComponentClassCreator.cs
@@ -0,0 +1,71 @@
+﻿using System;
+
+namespace Bridge.React
+{
+	internal static class ReactComponentClassCreator
+	{
+		public static object CreateClass(object template)
+		{
+			if (template == null)
+				throw new ArgumentNullException(nameof(template));
+
+			var displayName = ComponentNameHelpers.GetDisplayName(template);
+			object reactComponentClass = null;
+			/*@
+			// Use the displayName to name the component class function (React DevTools will use this)
+			eval("reactComponentClass = function " + displayName + "(props) { Bridge.React.ReactComponentClassCreator.initialiseComponentState(this, props); }");
+
+			// Set the React.Component base class
+			reactComponentClass.prototype = Object.create(
+				React.Component && React.Component.prototype,
+				{ constructor: { value: reactComponentClass, enumerable: false, writable: true, configurable: true } }
+			);
+			if (Object.setPrototypeOf) {
+				Object.setPrototypeOf(reactComponentClass, React.Component);
+			}
+			else {
+				reactComponentClass.__proto__ = React.Component;
+			}
+
+			// Attach the members
+			for (var name in template) {
+				if (name === 'constructor') {
+					continue;
+				}
+				var value = template[name];
+				var descriptor = {
+					value: value,
+					enumerable: false,
+					configurable: true,
+					writable: true
+				}
+				Object.defineProperty(reactComponentClass.prototype, name, descriptor);
+			}
+			*/
+			return reactComponentClass;
+		}
+
+		/// <summary>
+		/// Class-based components don't support the "getInitialState" method as the state (if an initial value must be set) may be set in the constructor. These bindings don't
+		/// support constructors (because there are too many complications with constructor overloading, which JavaScript doesn't have to deal with) and so we need a way to tie
+		/// the two concepts together. To do this, if a stateful component has a getInitialState method (renamed in the JavaScript to "constructorStateInitialiser" in these
+		/// bindings) then it will be called from the component constructor and used to set the initial state value.
+		/// </summary>
+		private static void InitialiseComponentState(object instance, object props)
+		{
+			// The props reference is passed to the constructor but won't yet have been applied to the instance - we'll need to do that in case the props need to be accessed
+			// from the GetInitialState method (it shouldn't matter that we're doing this early, React will do it itself shortly)
+			/*@
+				var getInitialState = instance.constructorStateInitialiser;
+				if ((typeof(getInitialState) !== "function") || (getInitialState.length !== 0)) {
+					return;
+				}
+				instance.props = props;
+				var state = getInitialState.call(instance);
+				if (state) {
+					instance.state = { value: state };
+				}
+			*/
+		}
+	}
+}
