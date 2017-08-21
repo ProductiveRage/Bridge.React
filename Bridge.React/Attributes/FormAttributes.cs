fe7658f8bb624cc862b09593100867eafc7c5216
ProductiveRage <dangger36@gmail.com>
2017-08-21 20:34:31 +0100

    Accept arbitrary strings for Form / Input "AutoComplete" property - fixes #31

--- Bridge.React/Attributes/FormAttributes.cs
+++ Bridge.React/Attributes/FormAttributes.cs
@@ -9,7 +9,7 @@ namespace Bridge.React
 		public string AcceptCharset { private get; set; }
 		public string Action { private get; set; }
 		[Name("autocomplete")]
-		public AutoComplete AutoComplete { private get; set; }
+		public Union<string, AutoComplete> AutoComplete { private get; set; }
 		public string Encoding { private get; set; }
 		public string Enctype { private get; set; }
 		public string Method { private get; set; }
