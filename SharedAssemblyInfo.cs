03c82c3292b6bdf5d9d2ce34e24f4b30be8a48ec
Dion Williams <dion.williams@newmind.co.uk>
2017-09-08 16:07:14 +0100

    Bump version to 3.1.0 for release

--- SharedAssemblyInfo.cs
+++ SharedAssemblyInfo.cs
@@ -1,5 +1,5 @@
 ﻿using System.Reflection;
 
 [assembly: AssemblyCopyright("Copyright © ProductiveRage 2017")]
-[assembly: AssemblyVersion("3.0.0.0")]
-[assembly: AssemblyFileVersion("3.0.0.0")]
+[assembly: AssemblyVersion("3.1.0.0")]
+[assembly: AssemblyFileVersion("3.1.0.0")]
