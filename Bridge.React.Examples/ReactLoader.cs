16c8621eba0e0cbca36cacf441dcaa1849cd6b2d
ProductiveRage <dangger36@gmail.com>
2018-06-08 10:53:44 +0100

    Use the Bridge.ReactLoader NuGet package to bring in the React library, rather than requiring script tags

---
+++ Bridge.React.Examples/ReactLoader.cs
@@ -0,0 +1,18 @@
+﻿namespace Bridge.ReactLoader.Application
+{
+	/// <summary>
+	/// This file is added to the project by the Bridge.ReactLoader NuGet package and should not be edited manually (if you update the NuGet package then this file will be replaced and so any
+	/// manual changes would be lost)
+	/// </summary>
+	internal static class LibraryLoader
+	{
+		private static void LoadAppropriateVersion()
+		{
+#if DEBUG
+			BridgeReactDebug.DebugBuild.Include();
+#else
+			BridgeReactRelease.ReleaseBuild.Include();
+#endif
+		}
+	}
+}
